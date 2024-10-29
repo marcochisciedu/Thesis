5e4466ac8970ebf2fce443b3ad7e656a8ae23c02
marcochisciedu <marco.chisci@edu.unifi.it>
2024-10-29 15:21:18 +0100

    Feature similarity

--- Experiments_CIFAR10/3D_plot.py
+++ Experiments_CIFAR10/3D_plot.py
@@ -3,6 +3,7 @@ import wandb
 import argparse, yaml
 from dotenv import load_dotenv
 import numpy as np
+from numpy import linalg
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 import torch
@@ -10,6 +11,7 @@ import plotly.graph_objs as go
 from airbench94 import CifarLoader, make_net, evaluate
 from scipy.spatial import ConvexHull
 import pandas as pd
+
 
 
 
@@ -65,6 +67,37 @@ def vector_plot(tvects,is_vect=True,orig=[0,0,0]):
 
     return fig
 
+# Calculates the adjecency matrix of the features
+def calculate_adjecency_matrix(W):
+    # Calculate the convex hull (Delaunay triangulation on the sphere)
+    hull = ConvexHull(W)
+
+    # Initialize the adjacency matrix
+    adjacency_matrix = np.zeros((W.shape[0], W.shape[0]), dtype=int)
+    
+    # Update the adjacency matrix based on the convex hull simplices
+    # Each simplex contains the indices of the 3 vertices it connects
+    for simplex in hull.simplices:
+        adjacency_matrix[simplex[0], simplex[1]] = 1
+        adjacency_matrix[simplex[1], simplex[0]] = 1
+        adjacency_matrix[simplex[1], simplex[2]] = 1
+        adjacency_matrix[simplex[2], simplex[1]] = 1
+        adjacency_matrix[simplex[2], simplex[0]] = 1
+        adjacency_matrix[simplex[0], simplex[2]] = 1
+
+    # Show the adjacency matrix
+    return adjacency_matrix
+
+# Get the squared frobenius norm of all the combinations of the adjacency matrices
+def frobenius_norm_matrices(matrices):
+    fro_norm_matrix = np.zeros((matrices.shape[0], matrices.shape[0]), dtype = int)
+
+    for i in range(matrices.shape[0]):
+        for j in range(matrices.shape[0]):
+            fro_norm_matrix[i][j] = linalg.norm(np.dot(matrices[i].T, matrices[j]),ord= 'fro')**2
+    
+    return fro_norm_matrix
+
 # Distance between all the vectors
 def vectors_distances(W):
     distances = np.zeros((W.shape[0], W.shape[0]))
@@ -76,16 +109,42 @@ def vectors_distances(W):
 
     return distances
 
-def mean_std_matrices(matrices):
-    means = np.zeros((matrices.shape[1], matrices.shape[2]))
-    std = np.zeros((matrices.shape[1], matrices.shape[2]))
+# Find the k nearest neighbors of each vector given their distances
+def find_knn_vectors(distances, k):
+    knn_vectors= np.empty((distances.shape[0]), dtype= object)
 
-    for i in range(matrices.shape[1]):
-        for j in range(matrices.shape[2]):
-            means[i][j]=np.mean(matrices[:, i, j])
-            std[i][j] = np.std(matrices[:, i ,j])
+    for i in range(distances.shape[0]):
+        # Find indices of the k nearest vector, do not include the self-distance
+        knn_vectors[i]= np.argpartition(distances[i], k+1)[1:k+1]
+    return knn_vectors
 
-    return means, std
+# Calculate the intersection between k nearest neighbors of all the features
+def mutual_knn_alignment_features(W_array, k):
+    knn_alignment = np.zeros((W_array.shape[0], W_array.shape[0]) ,dtype= float)
+    knn_vectors = []
+    # Find distances and knn vectors for each set of features
+    for i in range(W_array.shape[0]):
+        tmp_distances = vectors_distances(W_array[i])
+        knn_vectors.append(find_knn_vectors(tmp_distances, k))
+    
+    # Find intersection between each possible features couple
+    for row in range(W_array.shape[0]):
+        for col in range(W_array.shape[0]):
+            intersection_size = 0
+            total_size = 0
+            for feature in range(k):
+                for value in knn_vectors[row][feature]:
+                    if value in knn_vectors[col][feature]:
+                        intersection_size += 1
+                        total_size+= 1
+                    else:
+                        total_size +=1
+            knn_alignment[row][col] = intersection_size/ total_size
+    
+    return knn_alignment
+
+
+
 
 def main():
 
@@ -111,13 +170,14 @@ def main():
 
     wandb_run=wandb.init(
         project=WANDB_PROJECT,
-        name = "3D plot multiple "+ model_name[1:-4],
+        name = "3D plot multiple runs"+ model_name[1:-4],
         config=hyp)
     
     # Get test images
     test_loader = CifarLoader('cifar10', train=False, batch_size=2000)
 
-    matrices = []
+    adj_matrices = []
+    W_array = []
 
     for i in range(5):
         current_model_name = model_name+ str(i)
@@ -136,33 +196,37 @@ def main():
 
         # Get model last layer's weights and plot its vectors
         W = model[8].weight.detach().cpu().numpy().astype(np.float32)
+        W_array.append(W)
         figure= vector_plot(W)
         wandb.log({"3d plot" +  current_model_name[1:]: wandb.Plotly(figure)})
 
-        distances=vectors_distances(W)
-        matrices.append(distances)
+        # Calculate the adjecency matrix of the features
+        adjacency_matrix=calculate_adjecency_matrix(W)
+        adj_matrices.append(adjacency_matrix)
         classes = ['plane', 'car', 'bird', 'cat','deer', 'dog', 'frog', 'horse', 'ship', 'truck']
-        df_distances = pd.DataFrame(distances)
-        df_distances.columns = classes
-        df_distances.index = classes
+        df_adjacency = pd.DataFrame(adjacency_matrix)
+        df_adjacency.columns = classes
+        df_adjacency.index = classes
 
-        print(df_distances)
-        wandb.log({"Vector distances "+  current_model_name[1:]: wandb.Table(dataframe=df_distances)})
-    matrices = np.array(matrices)
-    means, std = mean_std_matrices(matrices)
-    df_means = pd.DataFrame(means)
-    df_means.columns = classes
-    df_means.index = classes
-
-    print(df_means)
-    wandb.log({"Matrices mean "+ model_name[1:-4]: wandb.Table(dataframe=df_means)})
-
-    df_std = pd.DataFrame(std)
-    df_std.columns = classes
-    df_std.index = classes
+        print(df_adjacency)
+        wandb.log({"Adjecency matrix "+  current_model_name[1:]: wandb.Table(dataframe=df_adjacency)})
     
-    print(df_std)
-    wandb.log({"Matrices std "+  model_name[1:-4]: wandb.Table(dataframe=df_std)})
+    # Get the frobenius norm matrix of all the adjecency matrices    
+    adj_matrices = np.array(adj_matrices)
+    fro_norm_matrix=frobenius_norm_matrices(adj_matrices)
+    df_fro_norm = pd.DataFrame(fro_norm_matrix)
+
+    print( df_fro_norm)
+    wandb.log({"Frobenius norm matrix "+  model_name[1:-4]: wandb.Table(dataframe=df_fro_norm)})
+
+    # Calculate mutual knn aligment between features
+    W_array = np.array(W_array)
+    for k in [2,3,4,5]:
+        knn_alignment= mutual_knn_alignment_features(W_array, k)
+        df_knn_alignment = pd.DataFrame(knn_alignment)
+        print("k:" + str(k))
+        print( df_knn_alignment)
+        wandb.log({"KNN alignment, k: "+ str(k) + " " +  model_name[1:-4]: wandb.Table(dataframe=df_knn_alignment)})
 
     wandb_run.finish()
     
